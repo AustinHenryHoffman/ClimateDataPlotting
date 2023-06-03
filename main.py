4bd667897ae19e6a0460bc673ede4d9f8d1c6912
DrTautology <austinhenryhoffman@gmail.com>
2023-06-03 15:28:51 -0500

    Main script

---
+++ main.py
@@ -0,0 +1,75 @@
+import utime
+import st7789
+import tft_config
+import vga2_bold_16x32 as bigFont
+import vga1_8x16 as smallFont
+from network import WLAN, STA_IF   # handles connecting to WiFi
+import urequests    # handles making and servicing network requests
+from machine import Pin, I2C
+import machine
+import json
+import gc
+import random
+import os
+
+tft = tft_config.config(3)
+gc.enable()
+gc.collect()
+tft = tft_config.config(1, buffer_size=30 * 2)
+
+# enable display and clear screen
+tft.init()
+
+
+class NetworkManager:
+    def __init__(self):
+        self.wlan = WLAN(STA_IF)
+        self.wlan.active(True)
+
+    def connect_to_network(self):
+        with open("./etc/network_config.json", "r") as file:
+            config_data = json.load(file)
+        network_info = config_data["network"]
+        ssid = network_info["ssid"]
+        password = network_info["network_password"]
+        self.wlan.connect(ssid, password)
+
+
+# Start networking
+network_manager = NetworkManager()
+network_manager.connect_to_network()
+utime.sleep(3)
+
+
+def main():
+    tft.png("MasterBedroom_111356030623.png.png", 0, 0, True)
+    # Replace 'http://<pi_ip_address>:5000/images' with the URL of your Flask app
+    """
+    url = 'http://192.168.1.4:5000/image'
+    # Send a GET request to the Flask app
+    response = urequests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Get the list of image URLs from the response
+        image_urls = response.json()
+        print(response.json())
+        # Iterate over the image URLs
+        for i, image_url in enumerate(image_urls):
+            # Send a GET request for each image URL
+            image_response = urequests.get(image_url)
+
+            # Check if the request for the image was successful
+            if image_response.status_code == 200:
+                # Save the received image to a file
+                with open(f'image_{i}.png', 'r') as f:
+                    f.write(image_response.content)
+                    tft.png(i, 160, 120)
+                print(f'Image {i} saved successfully.')
+            else:
+                print(f'Error occurred while retrieving image {i}:', image_response.status_code)
+    else:
+        print('Error occurred:', response.status_code)
+    """
+
+main()
